a3f1a78f70f3f3dace58206336e213755747f14e
YoStM <yo.stm@outlook.com>
2022-05-22 10:00:37 +0200

    Add auction research through api endpoint parameter. Add try-catch blocks in auction_routes.js

--- src/routes/auction_routes.js
+++ src/routes/auction_routes.js
@@ -1,114 +1,143 @@
 const { ValidationError } = require("sequelize");
 const { Auction } = require("../db/sequelize");
+const { Op } = require("sequelize");
 
 const getAllAuctions = (server) => {
-  server.get("/auctions", (req, res) => {
-    Auction.findAll()
-      .then((auctions) => {
-        res.status(200).json({ data: auctions });
-      })
-      .catch((error) => {
-        res.status(500).json({
-          message: "La liste des enchères n'a pas pu être récupérée.",
-          data: error,
+  server.get("/auctions", async (req, res) => {
+    try {
+      let auctions;
+      if (req.query.title) {
+        const TITLE = req.query.title;
+        if (TITLE.length < 3) {
+          return res.status(404).json({
+            message:
+              "Le termne de recherche doit contenir au moins 3 caractères.",
+          });
+        }
+        auctions = await Auction.findAll({
+          where: {
+            title: {
+              [Op.like]: `%${TITLE}%`,
+            },
+          },
+          order: ["title"],
         });
+        return res
+          .status(200)
+          .json({ data: auctions, results: auctions.length });
+      }
+
+      auctions = await Auction.findAll();
+      return res.status(200).json({ data: auctions, results: auctions.length });
+    } catch (error) {
+      return res.status(500).json({
+        message: "La liste des enchères n'a pas pu êre récupérée.",
+        data: error.message,
       });
+    }
   });
 };
 
 const getAuctionByPk = (server) => {
-  server.get("/auction/:id", (req, res) => {
-    const Id = parseInt(req.params.id);
-    return Auction.findByPk(Id)
-      .then((auction) => {
-        res.status(200).json({
-          message: `L'enchère ${auction.id} a été récupérée avec succès.`,
-          data: auction,
-        });
-      })
-      .catch((error) => {
-        res.status(500).json({
-          message:
-            "Impossible de récupérer la ressource demandée. Veuillez réessayer dans quelques instants.",
-          data: error,
-        });
+  server.get("/auction/:id", async (req, res) => {
+    try {
+      const Id = parseInt(req.params.id);
+      const auction = await Auction.findByPk(Id);
+      return res.status(200).json({ data: auction });
+    } catch (error) {
+      return res.status(500).json({
+        message:
+          "Impossible de récupérer la ressource demandée. Veuillez réessayer dans quelques instants.",
+        data: error,
       });
+    }
   });
 };
 
 const createAuction = (server) => {
-  server.post("/auction", (req, res) => {
-    const data = req.body;
-    return Auction.create(data)
-      .then((auction) => {
-        res.status(201).json({
-          message: `L'enchère [${auction.title}] a bien été enregistrée.`,
-          data: auction,
-        });
-      })
-      .catch((error) => {
-        res.status(500).json({
-          message:
-            "Impossible d'enregistrer l'enchère. Veuillez réessayer dans quelques instants.",
-          error: error.message,
-        });
+  server.post("/auction", async (req, res) => {
+    try {
+      const data = req.body;
+      const newAuction = await Auction.create(data);
+      return res.status(201).json({
+        message: `L'enchère [${newAuction.title}] a bien été enregistrée.`,
+        data: newAuction,
       });
+    } catch (error) {
+      res.status(500).json({
+        message:
+          "Impossible d'enregistrer l'enchère. Veuillez réessayer dans quelques instants.",
+        error: error.message,
+      });
+    }
   });
 };
 
 const updateAuction = (server) => {
-  server.put("/auction/:id", (req, res) => {
-    const ID = req.params.id;
-    Auction.update(req.body, { where: { id: ID } }).then((_) => {
-      return Auction.findByPk(ID)
-        .then((auction) => {
-          if (auction === null) {
-            return res
-              .status(404)
-              .json({ message: "L'enchère demandée n'éxiste pas." });
-          }
-          res.status(200).json({
-            message: `L'enchère [${auction.title}] a bien été modifiée.`,
-          });
-        })
-        .catch((error) => {
-          if (error instanceof ValidationError) {
-            return res
-              .status(400)
-              .json({ message: error.message, data: error });
-          }
-          res
-            .status(500)
-            .json({ message: "L'enchère n'a pas pu être modifiée." });
-        });
-    });
-  });
-};
-
-const destroyAuction = (server) => {
-  server.delete("/auction/:id", (req, res) => {
-    Auction.findByPk(req.params.id).then((auction) => {
-      if (auction === null) {
+  server.put("/auction/:id", async (req, res) => {
+    try {
+      const ID = req.params.id;
+      await Auction.update(req.body, { where: { id: ID } });
+      const updatedAuction = await Auction.findByPk(ID);
+      if (updatedAuction === null) {
         return res
           .status(404)
           .json({ message: "L'enchère demandée n'existe pas." });
       }
-      const AuctionDeleted = auction;
-      Auction.destroy({ where: { id: auction.id } })
-        .then(() => {
-          res.status(200).json({
-            message: `L'enchère [${AuctionDeleted.title}] a bien été supprimée.`,
-            data: AuctionDeleted,
-          });
-        })
-        .catch((error) => {
-          return res.status(500).json({
-            message:
-              "L'enchère n'a pas pu être supprimée. Réessayez ultérieurement.",
-            data: error,
-          });
-        });
-    });
+      res.status(200).json({
+        message: `L'enchère [${updatedAuction.title}] a bien été modifiée.`,
+      });
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        return res.status(400).json({ message: error.message, data: error });
+      }
+      res.status(500).json({ message: "L'enchère n'a pas pu être modifiée." });
+    }
+  });
+};
+
+const destroyAuction = (server) => {
+  server.delete("/auction/:id", async (req, res) => {
+    try {
+      const AuctionToDelete = await Auction.findByPk(req.params.id);
+      if (AuctionToDelete === null) {
+        return res
+          .status(404)
+          .json({ message: "L'enchère demandée n'existe pas." });
+      }
+      const deletedAuction = AuctionToDelete;
+      Auction.destroy({ where: { id: AuctionToDelete.id } });
+      res.status(200).json({
+        message: `L'enchère [${deletedAuction.title}] a bien été supprimée.`,
+        data: deletedAuction,
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "L'enchère n'a pas pu être supprimée.", data: error });
+    }
+    // Auction.findByPk(req.params.id).then((auction) => {
+    //   if (auction === null) {
+    //     return res
+    //       .status(404)
+    //       .json({ message: "L'enchère demandée n'existe pas." });
+    //   }
+    //   const AuctionDeleted = auction;
+    //   Auction.destroy({ where: { id: auction.id } })
+    //     .then(() => {
+    //       res.status(200).json({
+    //         message: `L'enchère [${AuctionDeleted.title}] a bien été supprimée.`,
+    //         data: AuctionDeleted,
+    //       });
+    //     })
+    //     .catch((error) => {
+    //       return res.status(500).json({
+    //         message:
+    //           "L'enchère n'a pas pu être supprimée. Réessayez ultérieurement.",
+    //         data: error,
+    //       });
+    //     });
+    // });
   });
 };
 
